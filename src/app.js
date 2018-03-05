62d86720454e50d9ffe0d487abea99e39cb41002
gusddm <gusddm@gmail.com>
2018-03-05 14:16:06 -0300

    added missing assets on screen

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import slamLogo from '../assets/summer-slam-logo.png';
 import liveSaturday from '../assets/live-saturday.png';
+import downArrow from '../assets/down-arrow.png';
 
 const slamLogoImg = document.getElementById('summer-slam-logo');
 slamLogoImg.src = slamLogo;
 
 const liveSaturdayImg = document.getElementById('live-saturday');
 liveSaturdayImg.src = liveSaturday;
+
+const downArrowImg = document.getElementById('down-arrow');
+downArrowImg.src = downArrow;
 
 document.querySelector( "#menu-toggle" )
   .addEventListener( "click", function() {
